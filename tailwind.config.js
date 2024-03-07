2f3d075f82772bc98b5468cf6aae889862bd254d
aftabalam507 <aftabalam1996jan@gmail.com>
2024-03-07 23:31:13 +0530

    working

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        inter:["Inter","sans-serif"],
+      },
+      colors:{
+        primary:"#035156"
+      }
+    },
   },
   plugins: [],
 };
